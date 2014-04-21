442f0f48461f20c435cbc266402ed9c58aa4fd24
dnszero <david@techafina.com>
2014-04-21 15:27:45 -0400

    Bumped dokku version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 vagrant up
-vagrant ssh -- "wget -qO- https://raw.github.com/progrium/dokku/v0.2.1/bootstrap.sh | sudo DOKKU_TAG=v0.2.1 bash"
+vagrant ssh -- "wget -qO- https://raw.github.com/progrium/dokku/v0.2.3/bootstrap.sh | sudo DOKKU_TAG=v0.2.3 bash"
 
 cat ~/.ssh/id_rsa.pub | vagrant ssh -- sudo sshcommand acl-add dokku ${USER}
 
